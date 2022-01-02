8a653d29b33c82437fed961165c5f62d68580c63
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2022-01-02 08:05:25 +0200

    initial commit

---
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -0,0 +1,129 @@
+package com.example.android.minipaint
+
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f // has to be float
+
+class MyCanvasView(context: Context) : View(context) {
+
+    // These are your bitmap and canvas for caching what has been drawn before.
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private lateinit var frame: Rect
+
+    private val backgroundColor = ResourcesCompat.getColor(
+        resources, R.color.colorBackground, null
+    )
+
+    // for holding the color to draw with and initialize it with the colorPaint resource
+    // you defined earlier
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    private var path = Path()
+
+    // for caching the x and y coordinates of the current touch event (the MotionEvent coordinates).
+    // Initialize them to 0f.
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    // add variables to cache the latest x and y values. After the user stops moving and
+    // lifts their touch,
+    // these are the starting point for the next path (the next segment of the line to draw).
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        // a new bitmap and canvas are created every time the function executes.
+        // You need a new bitmap, because the size has changed. However, this is a memory leak,
+        // leaving the old bitmaps around.
+        // To fix this, recycle extraBitmap before creating the next one.
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        // create an instance of Bitmap with the new width and height, which are the screen size,
+        // and assign it to extraBitmap. The third argument is the bitmap color configuration.
+        // ARGB_8888 stores each color in 4 bytes and is recommended.
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
+        // Calculate a rectangular frame around the picture.
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // the x and y coordinates (in pixels) of the top left corner,
+        // and null for the Paint, as you'll set that later.
+
+        // Note: The 2D coordinate system used for drawing on a Canvas is in pixels,
+        // and the origin (0,0) is at the top left corner of the Canvas.
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        // Draw a frame around the canvas.
+        canvas.drawRect(frame, paint)
+    }
+
+    // Reset the path, move to the x-y coordinates of the touch event
+    // (motionTouchEventX and motionTouchEventY) and assign currentX and currentY to that value.
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        // Reset the path so it doesn't get drawn again.
+        path.reset()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+}
